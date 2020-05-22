c0bba82e9ebd120a652b4a9cefeb75ecf02b78c7
Micah Alleyne <micah.alleyne@hgs.hiddengeniusproject.org>
2020-05-22 14:56:39 -0700

    Final Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :projects do
+    resources :tasks do
+    	member do 
+    		patch :complete
+    	end
+    end
+  end
+
+  root "projects#index"
 end
